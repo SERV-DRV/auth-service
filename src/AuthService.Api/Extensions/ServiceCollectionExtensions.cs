a4e3e75208611d8780bc88fea84274e67bb6515e
SERV-DRV <sderosa-2023220@kinal.edu.gt>
2026-02-23 11:07:36 -0600

    Crear la migracion de la base de datos

---
+++ src/AuthService.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+using AuthService.Domain.Entities;
+using AuthService.Domain.Constants;
+using AuthService.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+ 
+namespace AuthService.Api.Extensions;
+ 
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"))
+                .UseSnakeCaseNamingConvention());
+ 
+        services.AddHealthChecks();
+ 
+        return services;
+    }
+}
